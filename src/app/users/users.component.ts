3928cb36a3c62d4e5b6770239728822eb845a5ea
Kyler Johnson <kyler.johnson@bullhorn.com>
2019-12-16 09:46:09 -0500

    adding takeUntil implementation

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, HostListener  } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Subject } from 'rxjs';
 import { User } from '../user';
 import { UserService } from '../user.service';
 import { UserResponse } from '../user-response';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
@@ -11,6 +12,9 @@ import { UserResponse } from '../user-response';
 })
 export class UsersComponent implements OnInit {
 
+  private onDestroy$: Subject<void> = new Subject<void>();
+
+  usersSub: Subscription;
   users: User[];
 
   constructor(
@@ -18,9 +22,18 @@ export class UsersComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.userService
+    this.usersSub = this.userService
       .getUsers()
+      // takeUntil should always be the last operator in the pipe
+      // to avoid memory leaks
+      .pipe(takeUntil(this.onDestroy$))
       .subscribe((response: UserResponse) => this.users = response.results);
   }
 
+  @HostListener('window:beforeunload')
+  ngOnDestroy(): void {
+    // all observable streams complete automatically unsubscribing
+    // when OnDestroy runs
+    this.onDestroy$.next();
+  }
 }
